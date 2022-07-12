bacd9de2e390165b697b9e3c9a84bd2686662c89
Gabriel Nogueira <gab.nog94@gmail.com>
2022-07-12 15:44:21 -0300

    update preds

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import json
 import pickle
 import time
 
-# import ktrain
-# from ktrain import text as txt
+import ktrain
+from ktrain import text as txt
 
 from label_studio_sdk import Client
 
@@ -25,46 +25,53 @@ ls.check_connection()
 labeled_set   = ls.get_project(L_ID)
 unlabeled_set = ls.get_project(U_ID)
 
-# print("Downloading training data from Label Studio...")
-# tic = time.perf_counter()
-# export_tasks_CONLL(labeled_set)
-# toc = time.perf_counter()
-# print(f'Done![{toc - tic:0.4f}s]')
+print("Downloading training data from Label Studio...")
+tic = time.perf_counter()
+export_tasks_CONLL(labeled_set)
+toc = time.perf_counter()
+print(f'Done![{toc - tic:0.4f}s]')
 
-# (trn, val, preproc) = txt.entities_from_conll2003(TDATA,
-#                                                   val_filepath=VDATA,
-#                                                   verbose=0)
+(trn, val, preproc) = txt.entities_from_conll2003(TDATA,
+                                                  val_filepath=VDATA,
+                                                  verbose=0)
 
-# print("Criando modelo")
-# tic = time.perf_counter()
-# model_bertimbau = txt.sequence_tagger(
-#     'bilstm-bert', preproc, verbose=0,
-#     transformer_model='neuralmind/bert-base-portuguese-cased')
-# toc = time.perf_counter()
-# print(f'Done![{toc - tic:0.4f}s]')
+print("Criando modelo")
+tic = time.perf_counter()
+model_bertimbau = txt.sequence_tagger(
+    'bilstm-bert', preproc, verbose=0,
+    transformer_model='neuralmind/bert-base-portuguese-cased')
+toc = time.perf_counter()
+print(f'Done![{toc - tic:0.4f}s]')
 
-# print("Criando learner")
-# tic = time.perf_counter()
-# learner_bertimbau = ktrain.get_learner(
-#     model_bertimbau, train_data=trn,
-#     val_data=val, batch_size=128)
-# toc = time.perf_counter()
-# print(f'Done![{toc - tic:0.4f}s]')
+print("Criando learner")
+tic = time.perf_counter()
+learner_bertimbau = ktrain.get_learner(
+    model_bertimbau, train_data=trn,
+    val_data=val, batch_size=128)
+toc = time.perf_counter()
+print(f'Done![{toc - tic:0.4f}s]')
 
-# print("Treinando learner")
-# tic = time.perf_counter()
-# learner_bertimbau.fit(
-#     0.01, 1, cycle_len=5,
-#     checkpoint_folder='/tmp/saved_weights_bertimbau')
-# toc = time.perf_counter()
-# print(f'Done![{toc - tic:0.4f}s]')
+print("Treinando learner")
+tic = time.perf_counter()
+learner_bertimbau.fit(
+    0.01, 1, cycle_len=5,
+    checkpoint_folder='/tmp/saved_weights_bertimbau')
+toc = time.perf_counter()
+print(f'Done![{toc - tic:0.4f}s]')
 
-# predictor_bertimbau = ktrain.get_predictor(
-#     learner_bertimbau.model, preproc)
+predictor_bertimbau = ktrain.get_predictor(
+    learner_bertimbau.model, preproc)
 
 
+
+print("Baixando textos...")
 texts = export_tasks_text(unlabeled_set)
-print(texts)
+
+print("Realizando predições...")
+preds = predictor_bertimbau.predict(texts)
+with open('preds.pickle', 'wb') as f:
+    pickle.dump(preds, f)
+
 
 # data = [gen_json(texto, pred) for texto, pred in zip(textos, preds)]
 
