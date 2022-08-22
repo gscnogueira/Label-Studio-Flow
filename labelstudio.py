c3667f21ca2ec89bad82a4c0cd1220529a270255
Vitor <vitorvasconcelos05062000@gmail.com>
2022-08-22 17:58:00 -0300

    modularizando e ajustando função transfer_annotations

--- labelstudio.py
+++ labelstudio.py
@@ -1,12 +1,15 @@
 import requests
 import json
+import re
+from label_studio_sdk import Client
 
-class Project:
+class Project():
 
-    def __init__(self, server, project_id, token):
+    def __init__(self, project_id, server, token):
+        #super().__init__(ls, server, token)
         self.server = server
-        self.project_id = project_id
         self.token = token
+        self.project_id = project_id
 
     def export_tasks(self, export_type="CONLL2003", file='train.conll', snapshot=None):
 
@@ -57,13 +60,175 @@ class Project:
                                  json = data)
         print(response.text)
 
-    def test(self):
+    # def test(self):
 
-        response = requests.get(f'http://{self.server}/labelstudio/api/projects/{self.project_id}/export/formats',
-                                 headers={'Authorization': f'Token {self.token}'},
-                                 )
+    #     response = requests.get(f'http://{self.server}/labelstudio/api/projects/{self.project_id}/export/formats',
+    #                              headers={'Authorization': f'Token {self.token}'},
+    #                              )
 
-        print(response.text)
+    #     print(response.text)
 
 
 
+#class Utils:
+class ls_Client:
+    def __init__(self, server, token):
+        self.server = server
+        self.token = token
+        self.client = Client(url=server, api_key=token)
+        self.client.check_connection()
+
+    def gen_annotation(self, text, start, end, label):
+        return {"id": str(end),
+                "from_name": "label",
+                "to_name": "text",
+                "type":"labels",
+                "value":{
+                    'start':start,
+                    'end' : end,
+                    'text' : text[start:end],
+                    'score': 0.5,
+                    'labels' : [label]
+                }}
+
+    def get_result(self, texto, pred):
+
+        start = 0
+        end   = 0
+        result = []
+        prev_label='O'
+
+        while len(pred)>0 and end < len(texto):
+
+            pattern = re.compile('[\s\-]')
+            if re.match(pattern, texto[end]):
+                end+=1
+                continue
+
+            token, tag  = pred.pop(0)
+            label = tag[2:] if tag != 'O' else 'O'
+            pos = tag[0]
+
+
+            if (label!=prev_label or pos == 'B'):
+                if prev_label!='O':
+                    result.append(self.gen_annotation(text=texto,
+                                                start=start,
+                                                end=end,
+                                                label=prev_label))
+                start = end
+
+            end += len(token)
+            prev_label=label
+
+        if prev_label!='O':
+            result.append(self.gen_annotation(text=texto,
+                                        start=start,
+                                        end=end,
+                                        label=prev_label))
+        return result
+
+    def gen_json(self, text, prediction, model_version, id):
+
+        json_ = {"meta": {}, "data":{} , "predictions":[]}
+        json_["meta"]["id"] = id
+        json_["data"]["text"] = text
+
+        json_["predictions"].append({"model_version": model_version,
+                                    "result": self.get_result(text, prediction),
+                                    "score":0.9})
+        return json_
+
+    def export_tasks_CONLL(self, project):
+        export_type="CONLL2003"
+        response = project.make_request(
+            method='GET',
+            url=f'/api/projects/{project.id}/export?exportType={export_type}')
+        with open('train.conll', 'w') as f:
+            f.write(response.text)
+
+    def export_tasks_text(self, project):
+        response = project.make_request('get',
+                                        f'/api/projects/{project.id}/tasks',
+                                        {'page_size': -1})
+        tasks = response.json()
+        return [task['data']['text'] for task in tasks]
+
+
+    def get_tasks_ids(self, project):
+        response = project.make_request('get',
+                                        f'/api/projects/{project.id}/tasks',
+                                        {'page_size': -1})
+        tasks = response.json()
+        return [task['id']  for task in tasks]
+
+    def get_unlabeled_tasks(self, project):
+        response = project.make_request('get',
+                                        f'/api/projects/{project.id}/tasks',
+                                        {'page_size': -1})
+        tasks = response.json()
+        return [task['data']['text'] for task in tasks if not task['annotations']]
+
+    def get_labeled_tasks(self, project):
+        response = project.make_request('get',
+                                        f'/api/projects/{project.id}/tasks',
+                                        {'page_size': -1})
+        tasks = response.json()
+        return [task for task in tasks if task['is_labeled'] == True]
+
+    def get_all_tasks(self, project):
+        response = project.make_request('get',
+                                        f'/api/projects/{project.id}/tasks',
+                                        {'page_size': -1})
+        tasks = response.json()
+        return tasks
+
+    def get_unlabeled_tasks_ids(self, project):
+        response = project.make_request('get',
+                                        f'/api/projects/{project.id}/tasks',
+                                        {'page_size': -1})
+        tasks = response.json()
+        return [task['id'] for task in tasks if not task['annotations']]
+
+    def is_empty_project(self, project):
+        response = project.make_request('get',
+                                        f'/api/projects/{project.id}/tasks',
+                                        {'page_size': 1})
+        tasks = response.json()
+        return len(tasks)==0
+
+    def transfer_annotations(self, ls,labeled_tasks):
+        annotations = []
+        for labeled_task in labeled_tasks:
+            task_id = labeled_task['meta']['id']
+            for ann in labeled_task['annotations']:
+                annotation = {"result": ann['result'],
+                            "was_cancelled":ann['was_cancelled'],
+                            "ground_truth":ann['ground_truth'],
+                            "lead_time":ann['lead_time'],
+                            "task":task_id, 
+                            "completed_by":ann['completed_by']
+                            } 
+
+                get=ls.make_request('get',
+                                    f'/api/tasks/{task_id}/annotations/')
+
+                list_old_ann = get.json()
+                print(list_old_ann)
+
+                if(len(list_old_ann)<=0):
+                    response=ls.make_request('post',
+                                    f'/api/tasks/{task_id}/annotations/',
+                                    json=annotation)
+                else:
+                    for ann_get in list_old_ann:
+                        id_ann_get = ann_get['id']
+                        ls.make_request('delete',
+                                    f'/api/annotations/{id_ann_get}/')
+
+                    response=ls.make_request('post',
+                                    f'/api/tasks/{task_id}/annotations/',
+                                    json=annotation)
+
+                annotations.append(response.json())
+        return annotations
--- main.py
+++ main.py
@@ -1,21 +1,38 @@
+import argparse
+import configparser
 import datetime as dt
 import json
+from os.path import exists
 import pickle
 import time
 
-from ktrain import text as txt
-from label_studio_sdk import Client
-import ktrain
+from labelstudio import ls_Client
+from model_query import Models
 
-from utils import export_tasks_CONLL
-from utils import gen_json
-from utils import get_agreements
-from utils import get_unlabeled_tasks
-from utils import gen_predictors
-from utils import get_unlabeled_tasks_ids
-from utils import get_labeled_tasks
-from utils import is_empty_project
-from utils import transfer_annotations
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("config_file", help="path to configuration file")
+args = vars(parser.parse_args())
+config_file = args['config_file'] 
+
+if not exists(config_file):
+    print('ERROR: File does not exist')
+    exit(1)
+
+config = configparser.ConfigParser()
+config.read(config_file)
+user_config = config['USER']
+model_config = config['MODELS']
+
+LABEL_STUDIO_URL = user_config['label_studio_url']
+API_KEY =  user_config['label_studio_api_key']
+L_ID = user_config['labeled_project_id']
+U_ID = user_config['unlabeled_project_id']
+TDATA = user_config.get('train_path')
+
+models = [o for o in config.items('MODELS')
+          if o not in config.defaults().items()]
+
 
 LABEL_STUDIO_URL = 'http://164.41.76.30/labelstudio'
 API_KEY =  'bc36020e5d03487292cac63d82661daa12320042'
@@ -26,12 +43,11 @@ U_ID = '40'
 TDATA = 'train.conll'
 VDATA = 'train.conll'
 
-ls = Client(url=LABEL_STUDIO_URL, api_key=API_KEY)
-ls.check_connection()
+ls = ls_Client(server=LABEL_STUDIO_URL,token=API_KEY)
+models = Models()
 
-
-annotation_set = ls.get_project(L_ID)
-prediction_set = ls.get_project(U_ID)
+annotation_set = ls.client.get_project(L_ID)
+prediction_set = ls.client.get_project(U_ID)
 
 modelos = [('BERTimbau-1', 'neuralmind/bert-base-portuguese-cased'),
            ('BERTimbau-2', 'neuralmind/bert-base-portuguese-cased'),
@@ -51,47 +67,43 @@ while True:
 
         # Pegamos apenas as tasks anotadas:
         print(f"[{dt.datetime.now()}] Procurando por novas anotações...")
-        labeled_tasks = get_labeled_tasks(prediction_set)
+        labeled_tasks = ls.get_labeled_tasks(project=prediction_set)
 
-        if(len(labeled_tasks)<=0 and not is_empty_project(prediction_set)):
+        if(len(labeled_tasks)<=0 and not ls.is_empty_project(prediction_set)):
             print("Não foram encrontradas novas anotações.")
             continue
 
         if len(labeled_tasks):
             print(f"[{dt.datetime.now()}] Enviando anotações para projeto de treinamento...")
             # transfere anotações de um projeto para o outro
-            transfer_annotations(ls,labeled_tasks)
+            ann = ls.transfer_annotations(ls.client,labeled_tasks)
+            with open('temp1.json', 'w') as f:
+                json.dump(ann, f)
 
         print(f"[{dt.datetime.now()}] Baixando dados rotulados...")
-        export_tasks_CONLL(annotation_set)
+        ls.export_tasks_CONLL(annotation_set)
 
         print(f"[{dt.datetime.now()}] Treinando modelos...")
-        predictors = gen_predictors(modelos, train_filepath=TDATA, val_filepath=VDATA)
+        predictors = models.gen_predictors(modelos, train_filepath=TDATA, val_filepath=VDATA)
 
         print(f"[{dt.datetime.now()}] Baixando dados não rotulados...")
-        tasks_texts = get_unlabeled_tasks(annotation_set)
-        unlabeled_ids = get_unlabeled_tasks_ids(annotation_set)
+        tasks_texts = ls.get_unlabeled_tasks(annotation_set)
+        unlabeled_ids = ls.get_unlabeled_tasks_ids(annotation_set)
 
         print(f"[{dt.datetime.now()}] Realizando predições...")
         predictions = [predictor.predict(tasks_texts) for predictor in predictors]
 
         print(f"[{dt.datetime.now()}] Comparando predições...")
-        agreements = get_agreements(tasks_texts, predictions, unlabeled_ids)
-        tasks = [gen_json(**agreement) for agreement in agreements]
+        agreements = models.get_agreements(tasks_texts, predictions, unlabeled_ids)
+        tasks = [ls.gen_json(**agreement) for agreement in agreements]
 
         print(f"[{dt.datetime.now()}] Atualizando predições...")
+
         ########################################################
         # Update preventivo para manter predições realizadas durante o tempo de treinamento dos modelos
-        labeled_tasks_new = get_labeled_tasks(prediction_set)
-        # para evitar a adição de anotações repetidas
-        for i in range(len(labeled_tasks)):
-            for j in range(len(labeled_tasks_new)):
-                if(labeled_tasks_new[j]['id']==labeled_tasks[i]['id']):
-                    labeled_tasks_new.pop(j)
-                    break
-        # transfere anotações de um projeto para o outro
-        transfer_annotations(ls,labeled_tasks_new)
+        labeled_tasks_new = ls.get_labeled_tasks(prediction_set)
         ########################################################
+
         prediction_set.make_request('DELETE', f'api/projects/{U_ID}/tasks/')
         prediction_set.import_tasks(tasks)
 
